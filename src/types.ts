e0b0e18a01a7ad498a32aef155e32fc545225f6f
jubeiargh <buyukkakac@gmail.com>
2025-04-04 11:57:13 +0200

    adjust types

--- src/types.ts
+++ src/types.ts
@@ -9,6 +9,7 @@ export interface BasicArticle {
   sentiment?: string;
   confidence?: number;
   summary?: string;
+  images?: string[];
 }
 
 export interface Article extends BasicArticle {
@@ -27,7 +28,16 @@ export type GetArticleApiResponse = ApiResponse<Article>;
 // Types for Basic Article API
 export interface GetBasicArticlesParams {
   query: string; // Search query
-  source?: string; // Source of the article
+  /**
+   * @deprecated => use sources
+   * source of the articles
+   */
+  source?: string;
+  /**
+   * source of the articles, accepts multiple
+   * if you select sources than "includeAllSources" is not necessary
+   */
+  sources?: string[];
   /**
    *  Start date in (YYYY-MM-DD) or ISO Date string
    */
@@ -40,6 +50,17 @@ export interface GetBasicArticlesParams {
   order?: 'ASC' | 'DESC'; // Sort order
   pageSize?: number; // Results per page (1-1000)
   page?: number; // Page number
+
+  /**
+   * include sources that are less mainstream western financially relevant
+   * could be sources with more state controlled and propaganda
+   * or regions of earth where west doesn't give a f*ck about
+   */
+  includeAllSources?: boolean;
+
+  sourceRegion?: string;
+
+  alignment?: string;
 }
 
 export type GetExtendedArticlesParams = GetBasicArticlesParams;
@@ -47,6 +68,7 @@ export type GetExtendedArticlesParams = GetBasicArticlesParams;
 export interface GetArticlesWebSocketParams {
   query: string; // Search query
   source?: string; // Source of the article
+  sources?: string[]; // Source of the article
   language?: string; // Language, default is "en"
   extended: boolean;
 }
